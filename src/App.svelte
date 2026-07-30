da2160b3d966359a5542c04fc47af6e1ddcbf0bb
Manuel Esteban Morales Zuarez <manuesteban1990@gmail.com>
2026-07-30 14:51:49 -0400

    feat: a very basic setup

--- src/App.svelte
+++ src/App.svelte
@@ -1,1 +1,12 @@
+<script lang="ts">
+  let title = "Hello Svelte!";
+</script>
 
+<h1>{title}</h1>
+
+<style lang="scss">
+  h1 {
+    color: #ff3e00;
+    font-family: 'Arial', sans-serif;
+  }
+</style>
