91ce1d44630561dd4323091779889919166c7d5c
Manuel Esteban Morales Zuarez <manuesteban1990@gmail.com>
2026-07-30 15:57:55 -0400

    feat: var color in style

--- src/App.svelte
+++ src/App.svelte
@@ -9,14 +9,14 @@
   const color = 'orange'
 </script>
 
-<h1>{title}</h1>
+<h1 style="--color: {color}">{title}</h1>
 <img {...imgObj} loading={lazy ? 'lazy' : null} />
 <h2 style="color: {color}">Subtitle</h2>
 <h2 style:color>Subtitle 2</h2>
 
 <style lang="scss">
   h1 {
-    color: #ff3e00;
+    color: var(--color, #ff3e00);
     font-family: 'Arial', sans-serif;
   }
 </style>
