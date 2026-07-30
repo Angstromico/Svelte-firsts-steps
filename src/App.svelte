be013e3a98067f721baa38b720bd100dd294b1c9
Manuel Esteban Morales Zuarez <manuesteban1990@gmail.com>
2026-07-30 15:06:18 -0400

    refactor: spread obj in img

--- src/App.svelte
+++ src/App.svelte
@@ -1,11 +1,13 @@
 <script lang="ts">
   let title = "Hello Svelte!";
-  const source = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQwmyybA__TCR0LdCBW4i1-aNK8KyqIAZpMUoYg-rZ1qA&s=10";
-  const alt = 'gif'
+  const imgObj = {
+  src: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQwmyybA__TCR0LdCBW4i1-aNK8KyqIAZpMUoYg-rZ1qA&s=10",
+  alt: "gif"
+};
 </script>
 
 <h1>{title}</h1>
-<img src={source} alt={alt} />
+<img {...imgObj} />
 
 <style lang="scss">
   h1 {
