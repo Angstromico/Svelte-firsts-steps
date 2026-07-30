8ad682ab58087cda21f782e218a5f3c085ed289b
Manuel Esteban Morales Zuarez <manuesteban1990@gmail.com>
2026-07-30 14:55:24 -0400

    feat: img

--- src/App.svelte
+++ src/App.svelte
@@ -3,6 +3,7 @@
 </script>
 
 <h1>{title}</h1>
+<img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQwmyybA__TCR0LdCBW4i1-aNK8KyqIAZpMUoYg-rZ1qA&s=10" alt="gif">
 
 <style lang="scss">
   h1 {
