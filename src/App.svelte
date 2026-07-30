140516be83f9b261d1716b2c41b36bfc7f07e9e8
Manuel Esteban Morales Zuarez <manuesteban1990@gmail.com>
2026-07-30 15:42:34 -0400

    feat: loading attribute in image

--- src/App.svelte
+++ src/App.svelte
@@ -4,10 +4,12 @@
     src: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQwmyybA__TCR0LdCBW4i1-aNK8KyqIAZpMUoYg-rZ1qA&s=10',
     alt: 'gif',
   }
+
+  let lazy = false
 </script>
 
 <h1>{title}</h1>
-<img {...imgObj} />
+<img {...imgObj} loading={lazy ? 'lazy' : null} />
 
 <style lang="scss">
   h1 {
