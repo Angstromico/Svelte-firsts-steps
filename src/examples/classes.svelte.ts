fa6bfbc53dbd8e75556d965b47ae0f98fd10ff0a
Manuel Esteban Morales Zuarez <manuesteban1990@gmail.com>
2026-07-31 10:10:03 -0400

    feat(examples): todo class

---
+++ src/examples/classes.svelte.ts
@@ -0,0 +1,13 @@
+export class Todo {
+  done = $state(false)
+  text = $state('')
+
+  constructor(text: string) {
+    this.text = text
+  }
+
+  reset() {
+    this.text = ''
+    this.done = false
+  }
+}
