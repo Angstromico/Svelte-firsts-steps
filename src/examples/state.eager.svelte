ef0ebfc921cca15ea005f1e0bf3b8bfbedebd01b
Manuel Esteban Morales Zuarez <manuesteban1990@gmail.com>
2026-07-31 11:26:36 -0400

    feat(example): eager state handling

---
+++ src/examples/state.eager.svelte
@@ -0,0 +1,13 @@
+<script>
+  let pathname = '/'
+</script>
+
+<nav>
+  <a href="/" aria-current={$state.eager(pathname) === '/' ? 'page' : null}
+    >home</a
+  >
+  <a
+    href="/about"
+    aria-current={$state.eager(pathname) === '/about' ? 'page' : null}>about</a
+  >
+</nav>
