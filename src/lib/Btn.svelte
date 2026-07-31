cd34b1625ee36d90f881c6d532a01ddd754d827d
Manuel Esteban Morales Zuarez <manuesteban1990@gmail.com>
2026-07-31 10:09:35 -0400

    feat(lib): reusable button

---
+++ src/lib/Btn.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+  let {
+    text,
+    onClick = () => {},
+  }: {
+    text: string
+    onClick?: () => void
+  } = $props()
+</script>
+
+<button onclick={onClick}>
+  {text}
+</button>
