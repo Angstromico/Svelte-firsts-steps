d6f3a0cb5637c132139e7ae4e9e1ffc80380f7c9
Manuel Esteban Morales Zuarez <manuesteban1990@gmail.com>
2026-07-31 16:27:27 -0400

    learn: await derivates

---
+++ src/lib/GetLikes.svelte
@@ -0,0 +1,20 @@
+<script>
+  let { post, like } = $props()
+
+  let likes = $derived(post.likes)
+
+  async function onclick() {
+    // increment the `likes` count immediately...
+    likes += 1
+
+    // and tell the server, which will eventually update `post`
+    try {
+      await like()
+    } catch {
+      // failed! roll back the change
+      likes -= 1
+    }
+  }
+</script>
+
+<button {onclick}>🧡 {likes}</button>
--- src/lib/index.svelte.ts
+++ src/lib/index.svelte.ts
@@ -1,6 +1,7 @@
 import Btn from './Btn.svelte'
 import Counter from './Counter.svelte'
 import Double from './Double.svelte'
+import GetLikes from './GetLikes.svelte'
 import GetTotal from './GetTotal.svelte'
 
-export { Btn, Counter, Double, GetTotal }
+export { Btn, Counter, Double, GetLikes, GetTotal }
